f418f546f33699027fd23fc31f00103687e5167a
Andrey Litvinov <litwinoff@gmail.com>
2016-12-05 19:25:16 +0300

    XML

---
+++ ch03/src/main/java/com/apress/prospring/ch3/DeclareSpringComponents.java
@@ -0,0 +1,22 @@
+package com.apress.prospring.ch3;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class DeclareSpringComponents {
+
+	public static void main(String[] args) {
+
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		
+		ctx.load("META-INF/spring/app-context-xml.xml");
+		ctx.refresh();
+		
+		MessageProvider messageProvider = ctx.getBean("messageProvider", 
+				MessageProvider.class);
+		
+		System.out.println(messageProvider.getMessage());
+		
+
+	}
+
+}

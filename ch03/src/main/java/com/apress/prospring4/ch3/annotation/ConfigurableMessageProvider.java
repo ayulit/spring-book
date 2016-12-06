cd6af4b9ba8a57f7ff0e20ad6ab56eecd0053d3e
Andrey Litvinov <litwinoff@gmail.com>
2016-12-06 10:28:51 +0300

    Simple values injection using SpEL (Annotation)

--- ch03/src/main/java/com/apress/prospring4/ch3/annotation/ConfigurableMessageProvider.java
+++ ch03/src/main/java/com/apress/prospring4/ch3/annotation/ConfigurableMessageProvider.java
@@ -3,7 +3,6 @@ package com.apress.prospring4.ch3.annotation;
 import com.apress.prospring.ch3.MessageProvider;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service("messageProvider")
